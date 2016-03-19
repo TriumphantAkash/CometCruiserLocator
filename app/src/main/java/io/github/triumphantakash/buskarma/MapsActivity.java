413fb76bbceb2acf1b9a02533c6ee373d026eca5
Akash Chaturvedi <axc144430@utdallas.edu>
2016-03-19 03:26:31 -0500

    now shows user's location in the app also

--- app/src/main/java/io/github/triumphantakash/buskarma/MapsActivity.java
+++ app/src/main/java/io/github/triumphantakash/buskarma/MapsActivity.java
@@ -1,18 +1,32 @@
 package io.github.triumphantakash.buskarma;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private GoogleMap mMap;
+    private LatLng you;
+    Marker m;
+    MarkerOptions a;
+    private final String LOG_TAG="BusKarma";
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +36,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Connecting my GoogleApiClient to Location Services
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    protected void onStop(){
+        //Disconnect the client
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -40,8 +72,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng UTD = new LatLng(32.9843468, -96.7481245);
+        you = new LatLng(32.9843468, -96.7481248);
         mMap.addMarker(new MarkerOptions().position(UTD).title("Comet Cruiser"));
+        a = new MarkerOptions().position(you).title("you");
+        m = mMap.addMarker(a);
         mMap.animateCamera(CameraUpdateFactory.newLatLng(UTD));
         //mMap.
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+        startLocationUpdates();
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG, "GoogleApiClient connection is suspended");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+//Log.i(LOG_TAG, location.toString());
+        String[] strings = location.toString().split(" ");
+        String[] strings1 = strings[1].split(",");
+        you = new LatLng(Double.parseDouble(strings1[0]), Double.parseDouble(strings1[1]));
+        Log.i(LOG_TAG, you.toString());
+        if(m !=null) {
+          m.setPosition(you);
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(LOG_TAG, "GoogleApiClient connection has failed");
     }
 }
